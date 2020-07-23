84fc1af84fb59acac68aefa37ea1730e4716fecc
Ahasanul Hoque <ahoque24@gmail.com>
2020-07-23 19:35:54 -0400

    Meeting Odin Project requirements

---
+++ README.md
@@ -0,0 +1,5 @@
+This was completed for The Odin Project's Etch-a-Sketch project, found here https://www.theodinproject.com/courses/web-development-101/lessons/etch-a-sketch-project
+
+This project uses HTML, CSS, and Javascript, and was used as practice with DOM manipulation.
+
+When the page initially loads, a 16x16 grid is shown on screen. The user can "draw" on this grid by mousing over each square. By clicking on the reset button above the grid, the user can change the dimensions of the grid. This will also clear the grid.
--- scripts/script.js
+++ scripts/script.js
@@ -2,27 +2,23 @@ const grid = document.querySelector('#grid');
 const button = document.querySelector('button');
 
 makeGrid();
-
-
 let squares = document.querySelectorAll('.grid-square');
 
-
+//Below function creates the grid and has a default parameter of 16 to
+//create the initial grid upon page load
 function makeGrid (gridSize=16) {
     let columnCount = 'grid-template-columns:';
-    let size = 400 / gridSize;
-    console.log(size);
-    let totalSize = 0;
+    let size = (500 / gridSize) - 2;  //This will be size of each square
+                                      //Subtract 2 to account for 1px border
     for(let i=0; i<gridSize*gridSize; i++){
         let gridSquare = document.createElement('div');
         gridSquare.classList.add('grid-square');
         gridSquare.style.width = size + 'px';
         gridSquare.style.height = size + 'px';
-        totalSize += size;
         grid.appendChild(gridSquare);
-        if(i%gridSize == 0) columnCount = columnCount + ' auto';
-    }
-    console.log(totalSize);
-    grid.setAttribute('style',columnCount);
+        if(i%gridSize == 0) columnCount = columnCount + ' auto'; //If the end of a column
+    }                                                            //has been reached, add
+    grid.setAttribute('style',columnCount);                      //another column
 }
 
 
@@ -37,7 +33,7 @@ button.addEventListener('click', () => {
 });
 
 function resetGrid() {
-    let gridSize = prompt("How many squares will the grid have?");
+    let gridSize = prompt("How many squares will be on each side of the grid?");
     while(isNaN(gridSize)){
         gridSize =prompt("Please enter a number");
     }
@@ -48,6 +44,7 @@ function resetGrid() {
     drawAgain();
 }
 
+//This function listens for events on the redrawn grid
 function drawAgain() {
     squares.forEach((square) => {
         square.addEventListener('mouseover', () => {
